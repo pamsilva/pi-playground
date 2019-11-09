69a7d3c80473d0a4192966c4583f18a8c7955dda
Pedro Silva <ams.pedro@gmail.com>
2019-11-09 12:31:41 +0000

    a first version of a script

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from time import sleep
+
+from envirophat import leds
+
+
+def blink(duration):
+    leds.on()
+    sleep(duration)
+    leds.off()
+
+
+def blinker(duration, n):
+    for _ in range(n):
+        blink(duration)
+
+
 if __name__ == '__main__':
-    print('Hello potato')
+    blinker(0.5, 3)
